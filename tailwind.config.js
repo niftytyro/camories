549c5f388388d5dbe74df573e7b1e34da95866ec
Udasi Tharani <udasitharani@gmail.com>
2023-07-05 20:38:53 +0530

    complete half home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.hbs", "./**/*.hbs"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        serif: ['"Cormorant Garamond"', ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        ivory: "#F9F6F3",
+        biege: "#A3826C",
+      },
+    },
+    plugins: [require("@tailwindcss/typography")],
   },
-  plugins: [require("@tailwindcss/typography")],
 };
